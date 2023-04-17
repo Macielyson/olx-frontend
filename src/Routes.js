fc7ea0e4c953d6f6174fc0c8e266175b9c9e0971
Macielyson Nascimento <macielyson.s@gmail.com>
2023-04-17 14:06:43 -0300

    pagina 404

--- src/Routes.js
+++ src/Routes.js
@@ -1,13 +1,15 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Switch, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
+import NotFound from './pages/NotFound';
 
 export default () => {
     return (
         <Routes>
             <Route exact path="/" element={<Home />} />
             <Route exact path="/about" element={<About />} />
+            <Route path="*" element={<NotFound/>}/> {/*quando ela nao encontra nehuma ela manda essa*/}
         </Routes>
     )
 }
